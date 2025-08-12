cb7713a229d286a6ff0ab812db0c5119e7b2f437
Helamã VPC <helamacorrea@gmail.com>
2025-08-12 17:25:56 -0300

    discover page

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -19,6 +19,8 @@ function displayCompanies(companies) {
         let address = document.createElement('address');
         let description = document.createElement('p');
         let name = document.createElement('h2');
+        let button = document.createElement('button');
+        let companyDetails = document.createElement('dialog');
 
         card.setAttribute("class", "card");
         name.setAttribute("class", "name");
@@ -27,19 +29,62 @@ function displayCompanies(companies) {
         portrait.setAttribute("loading", "lazy");
         portrait.setAttribute("alt", `Logo image of ${company.name}`);
         portrait.setAttribute("width", "300");
-        portrait.setAttribute("height", "auto");
 
         card.setAttribute("id", company.id);
 
         address.textContent = company.address;
         description.textContent = company.description;
         name.textContent = company.name;
+        button.textContent = "Learn More"
+
+        button.addEventListener('click', () =>{
+            displayCompany(companyDetails, company);
+        });
+
 
         card.appendChild(name);
         card.appendChild(portrait);
         card.appendChild(address);
         card.appendChild(description);
+        card.appendChild(button);
+        card.appendChild(companyDetails);
         
         cards.appendChild(card);
     });
 }
+
+function displayCompany(companyDetails,company) {
+  companyDetails.innerHTML = '';
+  companyDetails.innerHTML = `
+    <button id="closeModal">❌</button>
+    <h2>${company.name}</h2>
+    <p><strong>Address</strong>: ${company.address}</p>
+    <p><strong>Description</strong>: ${company.description}</p>
+  `;
+  companyDetails.showModal();
+  
+  const closeModalB = companyDetails.querySelector('#closeModal');
+
+  closeModalB.addEventListener("click", () => {
+    companyDetails.close();
+  });
+}
+
+const messageEl = document.getElementById("visit-message");
+const lastVisit = localStorage.getItem("lastVisit");
+const now = Date.now();
+
+if (!lastVisit) {
+  messageEl.textContent = "Welcome! Let us know if you have any questions.";
+} else {
+  const daysDiff = Math.floor((now - lastVisit) / (1000 * 60 * 60 * 24));
+  if (daysDiff < 1) {
+    messageEl.textContent = "Back so soon! Awesome!";
+  } else if (daysDiff === 1) {
+    messageEl.textContent = `You last visited ${daysDiff} day ago.`;
+  } else {
+    messageEl.textContent = `You last visited ${daysDiff} days ago.`;
+  }
+}
+
+localStorage.setItem("lastVisit", now);
